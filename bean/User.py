7460885b644f85250be4e3fd4462e87a27fc1801
10swan <1259496020@qq.com>
2023-12-08 11:13:33 +0800

    #comment 数据请求方法完善

--- bean/User.py
+++ bean/User.py
@@ -8,7 +8,7 @@ class User:
         self.userId = userId
         self.LONGITUDE = LONGITUDE
         self.LATITUDE = LATITUDE
-        self.visited_neighbor = set()
+        self.visited_server = set()
 
     def get_user_nearest_server_location(self, adjacency_list):
         user_nearest_server = random.choice(list(adjacency_list.keys()))
@@ -20,14 +20,17 @@ class User:
                 user_nearest_server = SITE_ID
         return user_nearest_server
 
-    def request_data(self, dataId, nearest_server, adjacency_list):
-
-        for neighbor in adjacency_list[nearest_server].neighbors:
-            if neighbor not in self.visited_neighbor:
-                self.visited_neighbor.add(neighbor)
-                if neighbor.data[dataId]:
-                    print(f"找到了数据，它在{neighbor.SITE_ID}服务器")
-                else:
-                    self.request_data(dataId, neighbor[0], adjacency_list)
-                return neighbor
-        return "没有找到数据"
+    def request_data(self, dataId, nearest_server_SITE_ID, adjacency_list):
+        nearest_server = adjacency_list[nearest_server_SITE_ID]
+        if nearest_server not in self.visited_server:
+            self.visited_server.add(nearest_server)
+            if nearest_server[dataId]:
+                print(f"找到了数据，它在{nearest_server.SITE_ID}服务器")
+                return
+            else:
+                for neighbor in nearest_server.neighbors:
+                    if neighbor.data[dataId]:
+                        print(f"找到了数据，它在{neighbor.SITE_ID}服务器")
+                        return
+                self.request_data(dataId, nearest_server.neighbors[0], adjacency_list)
+                return "没有找到数据"
